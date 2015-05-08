969c4e14c8b9350301ec6c1bc9cd86d705d6742a
562208 <biswas.prasenjit@tcs.com>
2015-05-08 14:22:15 +0530

    rfghfghfg

---
+++ TestWebElasticSearch/src/com/servlet/FrontController.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+}
